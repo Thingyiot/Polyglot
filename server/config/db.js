beb2edc51a172d05b62554b918fb1cc629a41aba
scalewarrior <scalewarrior@gmail.com>
2015-05-11 10:20:12 -0700

    adding redis support

--- server/config/db.js
+++ server/config/db.js
@@ -2,11 +2,11 @@ module.exports = function(app) {
 
 var documentMapper=require('../libs/database/odm/odm');
 var relationalMapper=require('../libs/database/orm/orm');
+var keyValueMapper=require('../libs/database/key-value/keyValue');
 
 var dev=require('../config/env/development');
 
-
-var RedisClient = require('node-redis-client');
+var options={port:6379,host:'127.0.0.1',db:0};
 
 var mongo=new documentMapper();
 	mongo.connect(dev.mongo.uri);
@@ -15,5 +15,7 @@ var mongo=new documentMapper();
 var mysql=new relationalMapper();
     mysql.connect(dev.mysql.uri);
 
+var redis=new keyValueMapper();
+      redis.connect(options);
 
 }
